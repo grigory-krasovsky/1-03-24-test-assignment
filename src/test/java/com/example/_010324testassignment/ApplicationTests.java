e8c00213bc20b3a04e1eaba3407d1309609503e0
GrigoryKrasovsky <grigory.krasovsky@gmail.com>
2024-03-01 16:27:07 +0300

    initial commit

---
+++ src/test/java/com/example/_010324testassignment/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example._010324testassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
